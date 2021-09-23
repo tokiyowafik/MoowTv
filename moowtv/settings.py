f546bdcd61c1b8242f75e7dfa0d5f6324f0c4296
Clementine Rin <clementineRin09@gmail.com>
2021-09-23 10:28:43 +0700

    Change the default database to mysql.

--- moowtv/settings.py
+++ moowtv/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import keyring
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-muihopzofz)zti^ir%-%@r2ki)ibx(-fb&ewc9gy12@&lfjmff'
+SECRET_KEY = keyring.get_password("secret_key", "root")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'moowtv.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'moowtv',
+        'ROOT': 'localhost',
+        'USER': 'root',
+        'PASSWORD': keyring.get_password("database", "root")
     }
 }
 
